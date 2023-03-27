047dddaa751241e6ff99319304126f93d16ccc90
jkwebdeveloperr <jigukoshti3109@gmail.com>
2023-03-27 14:31:13 +0530

    done

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
 const Error404 = () => {
   return (
-    <div className="wrapper">
-      <h2 className="">Oops ! Page not found.</h2>
-      <img src={require('../assets/images/404.png')} alt="" />
-      <h4 className="">We Can't find the page you're looking for.</h4>
-      <Link to="/" className="kf-bts">
-        <span>GO BACK HOME</span>
-      </Link>
+    <div className="container">
+      <div className="error">
+        <h2 className="">Oops ! Page not found.</h2>
+        <img src={require('../assets/images/404.png')} alt="" />
+        <h4 className="">We Can't find the page you're looking for.</h4>
+        <Link to="/" className="kf-bts">
+          <span>GO BACK HOME</span>
+        </Link>
+      </div>
     </div>
   )
 }
